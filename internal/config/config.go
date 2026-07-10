9082580895f05108acf8ccec40821d41824c4857
Timur Gilyazov <progwayoftimur@gmail.com>
2026-07-10 15:52:47 +0500

    initialized config

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env        string `yaml:"env" env-default:"development"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"addr" env-default:"0.0.0.0:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("error openning config file: %s", err)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("error reading config file: %s", err)
+	}
+
+	return &cfg
+}
